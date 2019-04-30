48e11c55cdbc04bc206eacf4dec115862fbdf7f1
Primc <pablocruzmendez@gmail.com>
2019-04-30 17:26:02 +0200

    colors, pop up etc

--- cliente/Form1.cs
+++ cliente/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,8 +24,30 @@ namespace cliente
             string Description = TextBox_Razon.Text;
             Exception exc;
             if ((exc = new Status(Btn.Text, Description).Post()) != null) {
-                MetroFramework.MetroMessageBox.Show(this, message:exc.Message, height:120, title:"Error");
+                MetroFramework.MetroMessageBox.Show(this, message: exc.Message, height: 120, title: "Error");
             }
+            else if (Btn.Text == "Comida")
+            {
+                bool lmao = true;
+                int count = 60;
+                ThreadPool.QueueUserWorkItem((s) =>
+                {
+                    
+                    while (lmao)
+                    {
+                        Console.WriteLine("{0} minutos", count);
+                        Thread.Sleep(1000);
+                        count--;
+                    }
+                });
+                MetroFramework.MetroMessageBox.Show(this, message: (count + " minutos"), height: 120, title: "Comida");
+                lmao = false;
+            }
+            else if (Btn.Text != "Off")
+            {
+                MetroFramework.MetroMessageBox.Show(this, message: "Enviado", height: 120, title: "Post");
+            }
+            TextBox_Razon.Text = "";
         }
         private void Bt_comida_MouseEnter(object sender, EventArgs e)
         {
@@ -45,6 +68,11 @@ namespace cliente
             Button btn = sender as Button;
             btn.BackColor = Color.Green;
             btn.ForeColor = Color.LightGray;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
