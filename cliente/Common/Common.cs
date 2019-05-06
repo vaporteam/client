6b82fb246f19456df7eb6f73caefb836fd0b7188
Ruben Diaz <ruben.diaz@metrica.es>
2019-05-06 09:46:11 +0200

    Updated Common class

--- cliente/Common/Common.cs
+++ cliente/Common/Common.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +18,39 @@ namespace Common
 
     public class ClientConfig
     {
-        public int ActivityTimeout { get; set; } = 15 * 1000 * 60; // 15 Minutos
-        public int InputCheckRate { get; set; } = 5  * 1000 * 60; // 5 Min  
+        public int ActivityTimeout { get; set; } = Duration.Minute * 15; // 15 Minutos
+        public int InputCheckRate { get; set; } = Duration.Minute * 5; // 5 Min
+        public string Uri { get; set; } = "";
+    }
+
+    public static class Duration
+    {
+        public static int Second { get; } = 1000;
+        public static int Minute { get; } = Second * 60;
+        public static int Hour { get; } = Minute * 60;
+    }
+
+    public static class Utils
+    {
+        public static byte[] ExtractResource(Assembly assembly, string resourceName)
+        {
+            if (assembly == null)
+            {
+                return null;
+            }
+
+            using (Stream resFilestream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resFilestream == null)
+                {
+                    return null;
+                }
+
+                byte[] bytes = new byte[resFilestream.Length];
+                resFilestream.Read(bytes, 0, bytes.Length);
+
+                return bytes;
+            }
+        }
     }
 }
